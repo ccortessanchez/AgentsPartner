f0941585545d7256b87c2c8b4996c87f748e1eff
Carlos Cortés Sánchez <ccortes@gmv.es>
2017-07-03 14:59:18 +0200

    Added project files

---
+++ Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  Agents Partner
+//
+//  Created by User01 on 03/07/17.
+//  Copyright © 2017 Razeware LLC. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object {
+    dynamic var name = ""
+}
