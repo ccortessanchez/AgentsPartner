37af2db11b98c9e8e24bfaeedba168a7d54edd19
Carlos Cortés Sánchez <ccortes@gmv.es>
2017-07-04 08:50:00 +0200

    Logview updated with database specimens

--- AgentsPartner/LogViewController.swift
+++ AgentsPartner/LogViewController.swift
@@ -22,10 +22,11 @@
 
 import UIKit
 import MapKit
+import RealmSwift
 
 class LogViewController: UITableViewController {
   
-  var specimens = ["specimen1"]
+  var specimens = try! Realm().objects(Specimen).sorted(byKeyPath: "name", ascending: true)
   var searchResults = ["search1"]
   
   @IBOutlet weak var segmentedControl: UISegmentedControl!
@@ -92,6 +93,27 @@ extension LogViewController {
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = self.tableView.dequeueReusableCell(withIdentifier: "LogCell") as! LogCell
+    let specimen = specimens[indexPath.row]
+    
+    cell.titleLabel.text = specimen.name
+    cell.subtitleLabel.text = specimen.category.name
+    
+    switch specimen.category.name {
+        case "Uncategorized":
+            cell.iconImageView.image = UIImage(named: "IconUncategorized")
+        case "Reptiles":
+            cell.iconImageView.image = UIImage(named: "IconReptiles")
+        case "Flora":
+            cell.iconImageView.image = UIImage(named: "IconFlora")
+        case "Birds":
+            cell.iconImageView.image = UIImage(named: "IconBird")
+        case "Arachnids":
+            cell.iconImageView.image = UIImage(named: "IconArachnid")
+        case "Mammals":
+            cell.iconImageView.image = UIImage(named: "IconMammal")
+        default:
+            cell.iconImageView.image = UIImage(named: "IconUncategorized")
+    }
     
     return cell
   }
